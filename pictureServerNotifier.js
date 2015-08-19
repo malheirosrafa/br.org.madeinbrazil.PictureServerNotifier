0da3335e5ddedf9b1bb97b2d0c7df667f8f97880
candalo <lucassoares@firstsolution.com.br>
2015-08-19 11:24:33 -0300

    Agora quando recebe os elementos da fila, os elementos não permanecem nela

--- pictureServerNotifier.js
+++ pictureServerNotifier.js
@@ -9,7 +9,7 @@
 
 var amqp = require('amqplib/callback_api');
 
-const cacheServerIp = 'localhost';
+const cacheServerIp = 'pictures.embelezapp.com.br';
 
 function onError(error) {
 	console.log('problem with request: ' + error.message);
@@ -17,56 +17,55 @@ function onError(error) {
 
 
 amqp.connect('amqp://mqadmin:EmbelezApp2015@queue.embelezapp.com.br', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    
-    var ex = 'exchange';
+	conn.createChannel(function(err, ch) {
 
-    ch.assertExchange(ex, 'fanout', {durable: false});
-    
-    ch.assertQueue('', {exclusive: true}, function(err, q) {
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-	ch.bindQueue(q.queue, ex, '');
+    	var ex = 'exchange';
+
+    	ch.assertExchange(ex, 'fanout', {durable: false});
+
+    	ch.assertQueue('', {exclusive: true}, function(err, q) {
+        	console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+		ch.bindQueue(q.queue, ex, '');
 
     	ch.consume(q.queue, function(msg) {
-        
+
 		console.log(" [x] Received %s", msg.content.toString());
-   
-	        var http = require('http')
-		
-		var imagesPaths = ['/'+msg.content.toString()+'/normal.jpg', 
+
+	    var http = require('http');
+
+		var imagesPaths = ['/'+msg.content.toString()+'/normal.jpg',
                                    '/'+msg.content.toString()+'/thumb.jpg',
                                    '/'+msg.content.toString()+'/large.jpg',
                                    '/'+msg.content.toString()+'/medium.jpg',
                                    '/'+msg.content.toString()+'/small.jpg'
-                                  ]; 		
+                          ];
+
 
 		for(var index in imagesPaths) {
-	        	var options = {
-	    		    host: cacheServerIp,
+	        var options = {
+	    		host: cacheServerIp,
 	  		    port: 80,
 	  		    path: imagesPaths[index],
 	  		    method: 'PURGE'
-	        	};
-	
+	        };
+
  	  		var req = http.request(options, function(res) {
-   	        	    console.log('STATUS: ' + res.statusCode);
+   	        	console.log('STATUS: ' + res.statusCode);
 			    console.log('HEADERS: ' + JSON.stringify(res.headers));
 			    res.setEncoding('utf8');
 			    res.on('data', function (chunk) {
-		   		 console.log('BODY: ' + chunk);
-  	            	    });
-                	});
+		   			console.log('BODY: ' + chunk);
+  	            });
+            });
 
-        		req.on('error', onError);
+        	req.on('error', onError);
 
-        		//write data to request body
-        		req.write('data\n');
-        		req.write('data\n');
-        		req.end();
+        	//write data to request body
+        	req.write('data\n');
+        	req.write('data\n');
+        	req.end();
 		}
-   
-        }, {noAck: false});
-     });
+	  }, {noAck: true});
+    });
   });
 });
-
